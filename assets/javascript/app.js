2eadc4a004bd2f493e62f490434ff021a5040463
Chris B <cbega1227@gmail.com>
2022-11-17 16:46:18 -0800

    Fixed api error 404

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,18 +8,27 @@ var weatherIcon = document.getElementById("weather-icon");
 var temperature = document.getElementById("temperature");
 var windSpeeds = document.getElementById("wind-speeds");
 var humidity = document.getElementById("humidity");
-var weatherApi = "http://api.openweathermap.org/data/2.5/forecast?";
+var weatherApi = "https://api.openweathermap.org/data/2.5/forecast?";
 var apiKey = "898b277c6fc6f18c77b1aabe15516f58";
 var cardBody = document.querySelectorAll(".card-body");
 var buttonContainer = document.getElementById("recent-btn-container");
 
 // https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 // https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={API key}
-
 // fetchCoords function - responsible for fetching api data
+
+userForm.onsubmit = (e) => {
+  e.preventDefault();
+  var cityInput = userInput.value;
+  fetchCoordinates(cityInput);
+  renderButtons(cityInput);
+  userInput.value = "";
+  localStorage.setItem("previousButtons", JSON.stringify(previousButtons));
+};
+
 function fetchCoordinates(city) {
   // url end point for api
-  var rootEndPoint = "http://api.openweathermap.org/geo/1.0/direct";
+  var rootEndPoint = "https://api.openweathermap.org/geo/1.0/direct";
   // Concat url end point with needed query parameters
   // var apiCall = `${rootEndPoint}?q=${city}&appid=${apiKey}`;
   var apiCall = rootEndPoint + "?q=" + city + "&appid=" + apiKey;
@@ -77,14 +86,6 @@ function renderCards(data) {
 function renderDayForecast(data) {
   var iconUrl = `https://openweathermap.org/img/wn/${data.list[0].weather[0]["icon"]}.png`;
 
-  // // Dom manipulation
-  // var h1El = document.createElement("h1");
-  // var h1El2 = document.createElement("h1");
-  // var h1El3 = document.createElement("h1");
-  // var h1El4 = document.createElement("h1");
-  // var h1El5 = document.createElement("h1");
-  // var imgEl = document.createElement("img");
-
   cityName.textContent = data.city.name;
   date.textContent = data.list[0].dt_txt;
   temperature.textContent = data.list[0].main.temp;
@@ -97,15 +98,18 @@ const previousButtons = JSON.parse(localStorage.getItem("previousButtons")) || [
 function renderButtons(newBtn) {
   previousButtons.push(newBtn);
   const newButtonEl = document.createElement("button");
-  newButtonEl.classList.add("btn", "px-5", "bg-danger", "text-light", "w-100", "my-2");
+  newButtonEl.classList.add("btn", "px-5", "bg-danger", "text-light", "w-100", "my-2"); 
   newButtonEl.textContent = newBtn;
+  // newButtonEl.addEventListener("click", fetchCoordinates(cityInput));
   buttonContainer.append(newButtonEl);
 }
 previousButtons.forEach(renderButtons);
-function passedCities(e) {
-  var cityInput = this.textContent;
-  e.preventDefault();
-}
+// function passedCities(e) {
+//   e.preventDefault();
+//   var button = e.target();
+//   var cityInput = button.textContent;
+//   fetchCoordinates(cityInput);
+// }
 
 // This function is responsible for form submission and by capturing user input
 function localStorageSet(city) {
@@ -123,15 +127,6 @@ function localStoreGet(city) {
 
 var submitBtn = document.querySelector(".submit-btn");
 // userForm.addEventListener("submit", handleFormSubmit);
-
-userForm.onsubmit = (e) => {
-  e.preventDefault();
-  var cityInput = userInput.value;
-  fetchCoordinates(cityInput);
-  renderButtons(cityInput);
-  userInput.value = "";
-  localStorage.setItem("previousButtons", JSON.stringify(previousButtons));
-};
 
 // current and forecast weather - https://openweathermap.org/api/one-call-3
 // geocoding api - https://openweathermap.org/api/geocoding-api
