3c28ce3bcd834aa8288bd43f7ccb52f63fa9d444
Chris B <cbega1227@gmail.com>
2022-11-18 23:02:53 -0800

    Working on dynamically creating 5 day forecast cards.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,7 +11,7 @@ var windSpeeds = document.getElementById("wind-speeds");
 var humidity = document.getElementById("humidity");
 var weatherApi = "https://api.openweathermap.org/data/2.5/forecast?";
 var apiKey = "898b277c6fc6f18c77b1aabe15516f58";
-var cardBody = document.querySelectorAll(".card-body");
+var cardContainer = document.getElementById("card-container");
 var buttonContainer = document.getElementById("recent-btn-container");
 
 // https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
@@ -53,11 +53,6 @@ function fetchWeather(lat, lon) {
       return response.json();
     })
     .then(function (data) {
-      // // take the temp and lets display to the user as an h1
-
-      // // Create an h1 element dynamically
-      // // add text to that html element
-      // // append to dayForeCast
       renderDayForecast(data);
       renderCards(data);
     });
@@ -67,32 +62,44 @@ function fetchWeather(lat, lon) {
 
 // responsible for the dynamic creation of the cards based on the data the user inputs
 function renderCards(data) {
+  console.log(data);
+  let newDate = new Date(data.list[0].dt_txt);
+  let card = "";
   // var iconUrl = `https://openweathermap.org/img/wn/${data.list[i].weather[i]["icon"]}.png`;
   for (let i = 1; i < data.list.length; i += 8) {
-    var date = document.getElementById(`date${i}`);
-    var icon = document.getElementById(`icon${i}`);
-    var temp = document.getElementById(`temp${i}`);
-    var wind = document.getElementById(`wind${i}`);
-    var humidity = document.getElementById(`humidity${i}`);
+    let e = e[i];
+    console.log(e);
+    // var date = document.getElementById(`date${i}`);
+    // var icon = document.getElementById(`icon${i}`);
+    // var temp = document.getElementById(`temp${i}`);
+    // var wind = document.getElementById(`wind${i}`);
+    // var humidity = document.getElementById(`humidity${i}`);
 
     // date.textContent = data.list[i].dt_txt;
-    date.textContent = data.list[i].dt_txt;
-    temp.textContent = `Temperature: ${data.list[i].main.temp}`;
-    wind.textContent = `Wind Speed: ${data.list[i].wind.speed}`;
-    humidity.textContent = `Humidity: ${data.list[0].main.humidity} %`;
+    // date.textContent = data.list[i].dt_txt;
+    // temp.textContent = `Temperature: ${data.list[i].main.temp}`;
+    // wind.textContent = `Wind Speed: ${data.list[i].wind.speed}`;
+    // humidity.textContent = `Humidity: ${data.list[0].main.humidity} %`;
     // imgEl.src = iconUrl;
+
+    // Created elements
+    card += `<div>Test</div>`;
+    cardContainer.append(card);
+
+    // Update elements
+    // append elements
   }
 }
 
 function renderDayForecast(data) {
   var iconUrl = `https://openweathermap.org/img/wn/${data.list[0].weather[0]["icon"]}.png`;
-  let test = new Date(data.list[0].dt_txt);
+  let newDate = new Date(data.list[0].dt_txt);
 
   cityName.textContent = data.city.name;
-  date.textContent = test.toLocaleDateString("en-us", { year: "numeric", month: "numeric", day: "numeric" });
-  temperature.textContent = data.list[0].main.temp;
-  windSpeeds.textContent = data.list[0].main.humidity;
-  humidity.textContent = data.list[0].wind.speed;
+  date.textContent = newDate.toLocaleDateString("en-us", { year: "numeric", month: "numeric", day: "numeric" });
+  temperature.textContent = `Temp: ${data.list[0].main.temp}${"\u00B0F"}`;
+  windSpeeds.textContent = `Wind: ${data.list[0].main.humidity} MPH`;
+  humidity.textContent = `Humidity: ${data.list[0].wind.speed} %`;
   weatherIcon.src = iconUrl;
 }
 
@@ -110,30 +117,11 @@ buttonContainer.addEventListener("click", function (e) {
   fetchCoordinates(test);
 });
 
-// function passedCities(e) {
-//   e.preventDefault();
-//   var button = e.target();
-//   var cityInput = button.textContent;
-//   fetchCoordinates(cityInput);
-// }
-
-// This function is responsible for form submission and by capturing user input
-function localStorageSet(city) {
-  let recentSearch = [];
-  localStorage.setItem("city", city);
-  recentSearch.push(city);
-  console.log(city);
-}
-
-function localStoreGet(city) {
-  localStorage.getItem("city");
-  // renderButtons(city);
-}
-// Event listeners
-
 var submitBtn = document.querySelector(".submit-btn");
-clearBtn.addEventListener("onclick", function () {
-  localStorage.clear();
+clearBtn.addEventListener("click", function () {
+  console.log("test");
+  window.localStorage.clear();
+  buttonContainer.textContent = "";
 });
 // userForm.addEventListener("submit", handleFormSubmit);
 
