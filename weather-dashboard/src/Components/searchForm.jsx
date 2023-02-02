edbf257527ac6ddd156d886825c297c0d61837cb
Chris <cbega1227@gmail.com>
2023-02-02 13:51:40 -0700

    Added styling to form component.

--- weather-dashboard/src/Components/searchForm.jsx
+++ weather-dashboard/src/Components/searchForm.jsx
@@ -2,16 +2,21 @@ import React from "react";
 
 export default function searchForm() {
   return (
-    <form id="form-submit" className="flex flex-col items-center border">
-      <h2 class="py-3">Search for a City:</h2>
-      <input className="w-5/6 my-3 p-3 bg-indigo-100" placeholder="Search for a city..." type="text" aria-describedby="userInput" />
-      <button type="submit" class="w-5/6 py-3 text-base bg-green-600">
+    <form id="form-submit" className="flex flex-col items-center rounded-md bg-neutral-800 text-white shadow-lg shadow-black/70">
+      <h2 class="py-3 my-4 text-2xl">Search for a City:</h2>
+      <input
+        className="w-5/6 mb-10 p-3 rounded-md bg-neutral-500 placeholder-white"
+        placeholder="Search for a city..."
+        type="text"
+        aria-describedby="userInput"
+      />
+      <button type="submit" class="w-5/6 py-3 text-base  rounded-md bg-green-600 shadow-lg  hover:shadow-green-400/30">
         Submit
       </button>
-      <button class="w-5/6 my-3 py-3 bg-red-600" id="clear-btn">
+      <button class="w-5/6 my-3 py-3 rounded-md bg-red-600 shadow-lg hover:shadow-red-400/30" id="clear-btn">
         Clear
       </button>
-      <div id="recent-btn-container" class="w-5/6 my-3 py-3 text-center">
+      <div id="recent-btn-container" class="w-5/6 my-3 py-3 text-center rounded-md">
         Coming soon
       </div>
     </form>
